ab098d7b81984b9dc71278de2f59abdd437bb924
moulla <moulla@gmail.com>
2017-03-21 22:32:29 +0000

    modified ranklib from python

--- src/algorithm_evalutation/model_train_test.py
+++ src/algorithm_evalutation/model_train_test.py
@@ -12,29 +12,33 @@ RANKER = 1
 EPOCH = 20
 LAYER = 2
 NODE = 20
+def run_ranklib():
+    ### Call to Java to train model
 
-### Call to Java to train model
+    result= subprocess.run(["java", "-jar",
+                    "./RankLib_2p1/bin/RankLib.jar",
+                    "-train",TRAIN_INPUT,
+                    "-validate",VAL_INPUT,
+                    "-test",TEST_INPUT,
+                    "-ranker", str(RANKER),
+                    "-metric2t", 'ERR@2',
+                    "-epoch", str(EPOCH),
+                    "-layer", str(LAYER),
+                    "-node", str(NODE),
+                    "-save", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '.txt'], stdout=subprocess.PIPE)
 
-result= subprocess.run(["java", "-jar",
-                "./RankLib_2p1/bin/RankLib.jar",
-                "-train",TRAIN_INPUT,
-                "-validate",VAL_INPUT,
-                "-test",TEST_INPUT,
-                "-ranker", str(RANKER),
-                "-metric2t", 'ERR@2',
-                "-epoch", str(EPOCH),
-                "-layer", str(LAYER),
-                "-node", str(NODE),
-                "-save", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '.txt'], stdout=subprocess.PIPE)
+    print(result.stdout.decode())
 
-print(result.stdout.decode())
+    ### Call to Java to predict rank from trained model
 
-### Call to Java to predict rank from trained model
+    result= subprocess.run(["java", "-jar",
+                    "./RankLib_2p1/bin/RankLib.jar",
+                    "-load", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '.txt',
+                    "-rank",TEST_INPUT,
+                    "-score", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '_test_rank.txt'], stdout=subprocess.PIPE)
 
-result= subprocess.run(["java", "-jar",
-                "./RankLib_2p1/bin/RankLib.jar",
-                "-load", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '.txt',
-                "-rank",TEST_INPUT,
-                "-score", MODEL_DIR + MODELS[RANKER-1]+'_rn_epoch_' + str(EPOCH) + '_layer_' + str(LAYER) + '_node_' + str(NODE) + '_test_rank.txt'], stdout=subprocess.PIPE)
+    print(result.stdout.decode())
 
-print(result.stdout.decode())
+
+if __name__ == '__main__':
+    run_ranklib()
